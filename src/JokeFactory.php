163e5ecc1ae83136751d92de119eb7405e6a2d67
Amrit Man Shrestha <1658188+amritms@users.noreply.github.com>
2020-04-24 14:59:52 +0545

    Apply fixes from StyleCI (#1)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -12,7 +12,7 @@ class JokeFactory
         'In the Beginning there was nothing … then Chuck Norris roundhouse kicked nothing and told it to get a job.',
         'When God said, “Let there be light!” Chuck said, “Say Please.”',
         'Chuck Norris has a mug of nails instead of coffee in the morning.',
-        'If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.'
+        'If Chuck Norris were to travel to an alternate dimension in which there was another Chuck Norris and they both fought, they would both win.',
     ];
 
     public function __construct(array $jokes = null)
